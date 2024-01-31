63890e901739eafd3ef918974ae3414f05e8f484
Anurag <anuragnimkar21@gmail.com>
2024-01-31 19:25:00 +0530

    first commit

---
+++ Client/context/SocketProvider.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { io } from "socket.io-client";
+import { createContext, useContext, useMemo } from "react";
+
+
+const SocketContext = createContext(null);
+
+export const useSocket = () => {
+    const socket = useContext(SocketContext);
+    return socket;
+  };
+
+export const SocketProvider = (props) => {
+    const socket = useMemo(() => io("localhost:8000"), []);
+  
+    return (
+      <SocketContext.Provider value={socket}>
+        {props.children}
+      </SocketContext.Provider>
+    );
+  };
