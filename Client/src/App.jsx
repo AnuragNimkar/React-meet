f23c977ced1cf761869d6b5c448c6e54857d713f
Anurag <anuragnimkar21@gmail.com>
2024-02-02 17:07:22 +0530

    second phase

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from "react-router-dom";
+import Lobby from "../screens/Lobby";
+import RoomPage from "../screens/Room";
+
 import './App.css'
 
 function App() {
@@ -8,7 +9,11 @@ function App() {
 
   return (
     <>
-     
+     <Routes>
+
+      <Route path='/lobby' element = {<Lobby />}></Route>
+      <Route path="/room/:roomId" element={<RoomPage />} />
+     </Routes>
     </>
   )
 }
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { SocketProvider } from '../context/SocketProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter >
+    <SocketProvider>
+      <App/>
+    </SocketProvider>
+  </BrowserRouter>
   </React.StrictMode>,
 )
